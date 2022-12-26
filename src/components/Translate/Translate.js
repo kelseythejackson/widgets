f8514d81c2f02f1dbf1c2500dac3cd0fcf4a1e4f
Kelsey Jackson <kelseythej@gmail.com>
2022-12-26 02:17:03 -0600

    finished translate widget" "

--- src/components/Translate/Translate.js
+++ src/components/Translate/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from '../Dropdown/Dropdown';
+import Convert from '../Convert/Convert';
 
 const options = [
   {
@@ -11,21 +12,35 @@ const options = [
     value: 'ar',
   },
   {
-    label: 'Hinde',
+    label: 'Hindi',
     value: 'hi',
+  },
+  {
+    label: 'Dutch',
+    value: 'nl',
   },
 ];
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState('');
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
       <Dropdown
         options={options}
         selected={language}
         onSelectedChange={setLanguage}
         label="Select a language"
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert language={language} text={text} />
     </div>
   );
 };
