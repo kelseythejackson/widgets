8c8e365a742c5b721a0f2b96bcfaf529648939f3
Kelsey Jackson <kelseythej@gmail.com>
2022-12-27 16:04:22 -0600

    working on navigation

--- src/App.js
+++ src/App.js
@@ -1,44 +1,64 @@
 import React, { useState } from 'react';
 
-// import Accordion from "./components/Accordion/Accordion";
-// import Search from "./components/Search/Search";
-// import Dropdown from './components/Dropdown/Dropdown';
+import Accordion from './components/Accordion/Accordion';
+import Search from './components/Search/Search';
+import Dropdown from './components/Dropdown/Dropdown';
 import Translate from './components/Translate/Translate';
+import Route from './components/Route';
+import Header from './components/Navigation/Header';
 
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is a front end javascript framework",
-//   },
-//   {
-//     title: "Why use React?",
-//     content: "React is a favorite JS library among engineers",
-//   },
-//   {
-//     title: "How do you use React?",
-//     content: "You use React by creating components",
-//   },
-// ];
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
 
-// const options = [
-//   {
-//     label: 'The color red',
-//     value: 'red',
-//   },
-//   {
-//     label: 'The color green',
-//     value: 'green',
-//   },
-//   {
-//     label: 'A shade of blue',
-//     value: 'blue',
-//   },
-// ];
+const options = [
+  {
+    label: 'The color red',
+    value: 'red',
+  },
+  {
+    label: 'The color green',
+    value: 'green',
+  },
+  {
+    label: 'A shade of blue',
+    value: 'blue',
+  },
+];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
